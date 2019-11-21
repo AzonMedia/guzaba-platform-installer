207600ce5c960f6c0d7a29cb97ff024b057aa8a3
Vesko Kenashkov <vesko@azonmedia.com>
2019-11-21 10:25:58 +0200

    initial commit

---
+++ src/Plugin.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace GuzabaPlatform\Installer\Plugin;
+
+use Composer\Composer;
+use Composer\IO\IOInterface;
+use Composer\Plugin\PluginInterface;
+
+class Plugin implements PluginInterface
+{
+
+    /**
+     * Apply plugin modifications to Composer
+     *
+     * @param Composer $composer
+     * @param IOInterface $io
+     */
+    public function activate(Composer $Compoer, IOInterface $Io)
+    {
+        $Installer = new Installer($Io, $Compoer);
+        $Compoer->getInstallationManager()->addInstaller($Installer);
+    }
+}
