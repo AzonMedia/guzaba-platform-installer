a7b73af7ca144b59b9a817c9b6d8d3030d29b033
Vesko Kenashkov <vesko@azonmedia.com>
2019-11-21 12:09:50 +0200

    debug

--- src/Installer.php
+++ src/Installer.php
@@ -18,8 +18,10 @@ class Installer extends LibraryInstaller
     /**
      * This is the composer.type that is supported by this plugin
      */
-    protected const SUPPORTED_TYPE = 'guzaba-platform';
-    protected const SUPPORTED_PACKAGE = 'guzaba-platform/guzaba-platform';
+    protected const SUPPORTED_TYPES = [self::PACKAGE_TYPE_PLATFORM, self::PACKAGE_TYPE_COMPONENT];
+    protected const PACKAGE_TYPE_PLATFORM = 'guzaba-platform';
+    protected const PACKAGE_TYPE_COMPONENT = 'guzaba-platform-component';
+    //protected const SUPPORTED_PACKAGE = 'guzaba-platform/guzaba-platform';
 //    /**
 //     * {@inheritDoc}
 //     */
@@ -51,12 +53,27 @@ class Installer extends LibraryInstaller
 //        )
 //
 //    )
-        $package_name = $Package->getName();
+        $package_type = $Package->getType();
 
-        if ($package_name !== self::SUPPORTED_PACKAGE) {
-            return;//do not perform amy installation steps
+        if ($package_type === self::PACKAGE_TYPE_PLATFORM) {
+            $this->install_guzaba_platform($Repo, $Package);
+        } elseif ($package_type === self::PACKAGE_TYPE_COMPONENT) {
+            $this->install_guzaba_platform_component($Repo, $Package);
+        } else {
+            //throw
         }
 
+
+//        print 'name: '.$package_name.PHP_EOL;
+//        print 'dir: '.$target_dir.PHP_EOL;
+//        print 'autoload '.print_r($autoload, TRUE).PHP_EOL;
+        //$vendor_dir = $PackageEvent->getComposer()->getConfig()->get('vendor-dir');
+    }
+
+    private function install_guzaba_platform(InstalledRepositoryInterface $Repo, PackageInterface $Package) : void
+    {
+
+        $package_name = $Package->getName();
         print 'GuzabaPlatformInstaller running for '.$package_name.PHP_EOL;
 
         //$target_dir = $Package->getTargetDir();
@@ -97,15 +114,11 @@ class Installer extends LibraryInstaller
             'public_src_dir'    => $guzaba_platform_dir.'/app/public_src',
         ];
         file_put_contents($manifest_json_file, json_encode($manifest_content));
-        //$manifest_content = [];
-        //$manifest_
+    }
 
+    private function install_guzaba_platform_component(InstalledRepositoryInterface $Repo, PackageInterface $Package) : void
+    {
 
-
-//        print 'name: '.$package_name.PHP_EOL;
-//        print 'dir: '.$target_dir.PHP_EOL;
-//        print 'autoload '.print_r($autoload, TRUE).PHP_EOL;
-        //$vendor_dir = $PackageEvent->getComposer()->getConfig()->get('vendor-dir');
     }
 
     /**
@@ -115,6 +128,6 @@ class Installer extends LibraryInstaller
     {
         //return 'phpdocumentor-template' === $packageType;
         //print 'SUPPORTS: '.$package_type.PHP_EOL;
-        return self::SUPPORTED_TYPE === $package_type;
+        return in_array($package_type, self::SUPPORTED_TYPES);
     }
 }
