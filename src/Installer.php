004d47480f6ecc1ae3ead82fa99e6d5408fa8b36
Vesko Kenashkov <vesko@azonmedia.com>
2019-11-21 14:01:15 +0200

    - cleanup - fixed json generation

--- src/Installer.php
+++ src/Installer.php
@@ -22,38 +22,15 @@ class Installer extends LibraryInstaller
     protected const SUPPORTED_TYPES = [self::PACKAGE_TYPE_PLATFORM, self::PACKAGE_TYPE_COMPONENT];
     protected const PACKAGE_TYPE_PLATFORM = 'guzaba-platform';
     protected const PACKAGE_TYPE_COMPONENT = 'guzaba-platform-component';
-    //protected const SUPPORTED_PACKAGE = 'guzaba-platform/guzaba-platform';
-//    /**
-//     * {@inheritDoc}
-//     */
-//    public function getInstallPath(PackageInterface $package)
-//    {
-//        $prefix = substr($package->getPrettyName(), 0, 23);
-//        if ('phpdocumentor/template-' !== $prefix) {
-//            throw new \InvalidArgumentException(
-//                'Unable to install template, phpdocumentor templates '
-//                .'should always start their package name with '
-//                .'"phpdocumentor/template-"'
-//            );
-//        }
-//
-//        return 'data/templates/'.substr($package->getPrettyName(), 23);
-//    }
+
+    protected const JSON_ENCODE_FLAGS = JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES;
+
 
     public function install(InstalledRepositoryInterface $Repo, PackageInterface $Package)
     {
 
         parent::install($Repo, $Package);
-//        name: guzaba-platform/guzaba-platform
-//dir
-//autoload Array
-//    (
-//        [psr-4] => Array
-//        (
-//            [GuzabaPlatform\] => app/src/GuzabaPlatform
-//        )
-//
-//    )
+
         $package_type = $Package->getType();
 
         if ($package_type === self::PACKAGE_TYPE_PLATFORM) {
@@ -61,14 +38,9 @@ class Installer extends LibraryInstaller
         } elseif ($package_type === self::PACKAGE_TYPE_COMPONENT) {
             $this->install_guzaba_platform_component($Repo, $Package);
         } else {
-            //throw
+            throw new \RuntimeException(sprintf('An unsupported package type %s is provided.', $package_type));
         }
 
-
-//        print 'name: '.$package_name.PHP_EOL;
-//        print 'dir: '.$target_dir.PHP_EOL;
-//        print 'autoload '.print_r($autoload, TRUE).PHP_EOL;
-        //$vendor_dir = $PackageEvent->getComposer()->getConfig()->get('vendor-dir');
     }
 
     /**
@@ -79,14 +51,12 @@ class Installer extends LibraryInstaller
     private function install_guzaba_platform(InstalledRepositoryInterface $Repo, PackageInterface $Package) : void
     {
 
-        print sprintf('GuzabaPlatformInstaller initializing GuzabaPlatofrm').PHP_EOL;
+        print sprintf('GuzabaPlatformInstaller: initializing GuzabaPlatofrm').PHP_EOL;
 
         $installer_dir = __DIR__;
         $composer_json_dir = realpath($installer_dir.'/../../../../');//this is the root dir
-        //$guzaba_platform_dir = realpath($installer_dir.'/../../guzaba-platform/');
         $guzaba_platform_dir = $this->getInstallPath($Package);
         if (file_exists($composer_json_dir.'/app')) {
-            //error
             throw new \RuntimeException(sprintf('The directory %s already exists.', $composer_json_dir.'/app'));
         } else {
             `mkdir $composer_json_dir/app`;
@@ -110,7 +80,7 @@ class Installer extends LibraryInstaller
         $manifest_content->name = 'GuzabaPlatform';
         $manifest_content->url = 'https://platform.guzaba.org/';
         $manifest_content->components = [];
-        file_put_contents($manifest_json_file, json_encode($manifest_content, JSON_PRETTY_PRINT));
+        file_put_contents($manifest_json_file, json_encode($manifest_content, self::JSON_ENCODE_FLAGS ));
 
         $this->install_guzaba_platform_component($Repo, $Package);
 
@@ -119,12 +89,10 @@ class Installer extends LibraryInstaller
     private function install_guzaba_platform_component(InstalledRepositoryInterface $Repo, PackageInterface $Package) : void
     {
 
-        //print 'P1 : '.$this->getInstallPath($Package).PHP_EOL;// /home/local/PROJECTS/guzaba2-platform/Z6/vendor/guzaba-platform/guzaba-platform
-        //print 'P2 : '.$this->getPackageBasePath($Package).PHP_EOL;// /home/local/PROJECTS/guzaba2-platform/Z6/vendor/guzaba-platform/guzaba-platform
         $plugin_dir = $this->getInstallPath($Package);
 
         $package_name = $Package->getName();
-        print sprintf('GuzabaPlatformInstaller running for component $s', $package_name).PHP_EOL;
+        print sprintf('GuzabaPlatformInstaller: installing component %s', $package_name).PHP_EOL;
 
         //$target_dir = $Package->getTargetDir();
         $autoload = $Package->getAutoload();
@@ -139,19 +107,6 @@ class Installer extends LibraryInstaller
 
         $namespace = array_key_first($autoload['psr-4']);
 
-//        $installer_dir = __DIR__;
-//        $composer_json_dir = realpath($installer_dir.'/../../../../');//this is the root dir
-//        $guzaba_platform_dir = realpath($installer_dir.'/../../guzaba-platform/');
-//        if (file_exists($composer_json_dir.'/bin')) {
-//            //error
-//        } else {
-//            `mkdir $composer_json_dir/app`;
-//            `cp -r $guzaba_platform_dir/app/bin $composer_json_dir/app/bin`;
-//            `cp -r $guzaba_platform_dir/app/certificates $composer_json_dir/certificates`;
-//            `mkdir $composer_json_dir/app/public_src`;
-//            `mkdir $composer_json_dir/app/public_src/build`;
-//            //`ln -s $guzaba_platform_dir/app/public_src $composer_json_dir/public_src/$namespace`;
-//        }
         $installer_dir = __DIR__;
         $composer_json_dir = realpath($installer_dir.'/../../../../');//this is the root dir
         $manifest_json_file = $composer_json_dir.'/manifest.json';
@@ -170,7 +125,7 @@ class Installer extends LibraryInstaller
         $component->src_dir = $plugin_dir.'/app/src';
         $component->public_src_dir = $plugin_dir.'/app/public_src';
         $manifest_content->components[] = $component;
-        file_put_contents($manifest_json_file, json_encode($manifest_content, JSON_PRETTY_PRINT));
+        file_put_contents($manifest_json_file, json_encode($manifest_content, self::JSON_ENCODE_FLAGS ));
 
         //update the webpack.config.js
     }
