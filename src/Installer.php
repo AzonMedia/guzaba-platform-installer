a22eb2d7c2c83de81ce25301b0339567375be0bb
Vesko Kenashkov <vesko@azonmedia.com>
2019-11-21 11:56:43 +0200

    debug

--- src/Installer.php
+++ src/Installer.php
@@ -19,6 +19,7 @@ class Installer extends LibraryInstaller
      * This is the composer.type that is supported by this plugin
      */
     protected const SUPPORTED_TYPE = 'guzaba-platform';
+    protected const SUPPORTED_PACKAGE = 'guzaba-platform/guzaba-platform';
 //    /**
 //     * {@inheritDoc}
 //     */
@@ -38,17 +39,69 @@ class Installer extends LibraryInstaller
 
     public function install(InstalledRepositoryInterface $Repo, PackageInterface $Package)
     {
-        print 'INSTALL'.PHP_EOL;
-
 
         parent::install($Repo, $Package);
-
+//        name: guzaba-platform/guzaba-platform
+//dir
+//autoload Array
+//    (
+//        [psr-4] => Array
+//        (
+//            [GuzabaPlatform\] => app/src/GuzabaPlatform
+//        )
+//
+//    )
         $package_name = $Package->getName();
-        $target_dir = $Package->getTargetDir();
+        //$target_dir = $Package->getTargetDir();
         $autoload = $Package->getAutoload();
-        print 'name: '.$package_name.PHP_EOL;
-        print 'dir '.$target_dir.PHP_EOL;
-        print 'autoload '.print_r($autoload, TRUE).PHP_EOL;
+        if (!isset($autoload['psr-4'])) {
+            //error
+        }
+        //if more than one ns - error too
+
+        $namespace = $autoload['psr-4'][0];
+
+        if ($package_name !== self::SUPPORTED_PACKAGE) {
+            return;//do not perform amy installation steps
+        }
+
+        $installer_dir = __DIR__;
+        $composer_json_dir = realpath($installer_dir.'/../../../../');//this is the root dir
+        $guzaba_platform_dir = realpath($installer_dir.'/../../guzaba-platform/');
+        if (file_exists($composer_json_dir.'/bin')) {
+            //error
+        } else {
+            `cp -r $guzaba_platform_dir/app/bin $composer_json_dir/bin`;
+            `cp -r $guzaba_platform_dir/app/certificates $composer_json_dir/certificates`;
+            `mkdir $composer_json_dir/public_src`;
+            `mkdir $composer_json_dir/public_src/build`;
+            //`ln -s $guzaba_platform_dir/app/public_src $composer_json_dir/public_src/$namespace`;
+        }
+        $manifest_json_file = $composer_json_dir.'/manifest.json';
+        if (file_exists($manifest_json_file)) {
+            $manifest_content = json_decode(file_get_contents($manifest_json_file));
+        } else {
+            $manifest_content = [];
+        }
+        if (!array_key_exists('components',$manifest_content)) {
+            $manifest_content['components'] = [];
+        }
+        $manifest_content['components'][] = [
+            'name'              => $package_name,
+            'namespace'         => $namespace,
+            'root_dir'          => $guzaba_platform_dir,
+            'src'               => $guzaba_platform_dir.'/app/src',
+            'public_src_dir'    => $guzaba_platform_dir.'/app/public_src',
+        ];
+        file_put_contents($manifest_json_file, json_encode($manifest_content));
+        //$manifest_content = [];
+        //$manifest_
+
+
+
+//        print 'name: '.$package_name.PHP_EOL;
+//        print 'dir: '.$target_dir.PHP_EOL;
+//        print 'autoload '.print_r($autoload, TRUE).PHP_EOL;
         //$vendor_dir = $PackageEvent->getComposer()->getConfig()->get('vendor-dir');
     }
 
