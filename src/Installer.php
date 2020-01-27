dd4f16b3e8984433cf274965a4cfcfd2a5bdd6bd
Peter <peter@azonmedia.com>
2020-01-27 12:16:43 +0200

    m

--- src/Installer.php
+++ src/Installer.php
@@ -9,6 +9,8 @@ use Composer\Repository\InstalledRepositoryInterface;
 use GuzabaPlatform\Installer\Interfaces\PostInstallHookInterface;
 use GuzabaPlatform\Installer\Interfaces\PostUninstallHookInterface;
 use GuzabaPlatform\Installer\Interfaces\PostUpdateHookInterface;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Component\Process\Process;
 
 /**
  * Class Installer
@@ -121,6 +123,7 @@ class Installer extends LibraryInstaller
         $installer_dir = __DIR__;
         $composer_json_dir = realpath($installer_dir.'/../../../../');//this is the root dir
         $guzaba_platform_dir = $this->getInstallPath($Package);
+
         if (file_exists($composer_json_dir.'/app')) {
             //throw new \RuntimeException(sprintf('The directory %s already exists.', $composer_json_dir.'/app'));
         } else {
@@ -187,6 +190,21 @@ class Installer extends LibraryInstaller
         }
         $this->install_guzaba_platform_component($Repo, $Package);
 
+        $database_file = $guzaba_platform_dir . '/app/database/guzaba2.sql';
+        $import_script = $guzaba_platform_dir . '/app/database/import_database.sh';
+        $env_file = $guzaba_platform_dir . '/app/dockerfiles/GuzabaPlatform/guzaba-platform.env';
+
+        if (file_exists($database_file) && is_readable($database_file) && file_exists($import_script) && is_executable($import_script)) {
+            if (\version_compare(\PHP_VERSION, '7.4.0', '>=')) {
+                $process = new Process([ $import_script, $database_file, $env_file ]);
+            } else {
+                $process = new Process($import_script . ' ' . $database_file . ' ' . $env_file);
+            }
+
+            $process->run(function ($type, $buffer) {
+                echo $buffer;
+            });
+        }
     }
 
     private function install_guzaba_platform_component(InstalledRepositoryInterface $Repo, PackageInterface $Package) : void
